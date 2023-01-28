8a9521c17f0526e07a8a2ea9e1b857893c33ae12
Ectimel <dtworek94@gmail.com>
2023-01-28 19:13:09 +0000

    added rest data dependency and get rid of boilerplate code

--- src/main/java/com/ectimel/springbootrestapihibernate/dao/EmployeeRepository.java
+++ src/main/java/com/ectimel/springbootrestapihibernate/dao/EmployeeRepository.java
@@ -2,8 +2,8 @@ package com.ectimel.springbootrestapihibernate.dao;
 
 import com.ectimel.springbootrestapihibernate.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource (path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
--- src/main/java/com/ectimel/springbootrestapihibernate/rest/RController.java
+++ src/main/java/com/ectimel/springbootrestapihibernate/rest/RController.java
@@ -1,47 +1,47 @@
-package com.ectimel.springbootrestapihibernate.rest;
-
-import com.ectimel.springbootrestapihibernate.entity.Employee;
-import com.ectimel.springbootrestapihibernate.service.EmployeeServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController()
-public class RController {
-
-    @Autowired
-    private EmployeeServiceImpl employeeService;
-
-    @GetMapping("/")
-    public String home(){
-        return "Hello RestAPI";
-    }
-
-    @GetMapping("/employees")
-    public List<Employee> getEmployees(){
-        return employeeService.findAll();
-    }
-
-    @GetMapping("/employee/{employeeId}")
-    public Employee getEmployee(@PathVariable int employeeId){
-        return employeeService.findById(employeeId);
-    }
-
-    @DeleteMapping("/employee/{employeeId}")
-    public void deleteEmployee(@PathVariable int employeeId){
-        employeeService.deleteById(employeeId);
-    }
-
-    @PutMapping("/employee")
-    public void updateEmployee(@RequestBody Employee employee){
-        employeeService.save(employee);
-    }
-
-    @PostMapping("/employee")
-    public void saveEmployee(@RequestBody Employee employee){
-        employee.setId(0);
-        employeeService.save(employee);
-    }
-
-}
+//package com.ectimel.springbootrestapihibernate.rest;
+//
+//import com.ectimel.springbootrestapihibernate.entity.Employee;
+//import com.ectimel.springbootrestapihibernate.service.EmployeeServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController()
+//public class RController {
+//
+//    @Autowired
+//    private EmployeeServiceImpl employeeService;
+//
+//    @GetMapping("/")
+//    public String home(){
+//        return "Hello RestAPI";
+//    }
+//
+//    @GetMapping("/employees")
+//    public List<Employee> getEmployees(){
+//        return employeeService.findAll();
+//    }
+//
+//    @GetMapping("/employee/{employeeId}")
+//    public Employee getEmployee(@PathVariable int employeeId){
+//        return employeeService.findById(employeeId);
+//    }
+//
+//    @DeleteMapping("/employee/{employeeId}")
+//    public void deleteEmployee(@PathVariable int employeeId){
+//        employeeService.deleteById(employeeId);
+//    }
+//
+//    @PutMapping("/employee")
+//    public void updateEmployee(@RequestBody Employee employee){
+//        employeeService.save(employee);
+//    }
+//
+//    @PostMapping("/employee")
+//    public void saveEmployee(@RequestBody Employee employee){
+//        employee.setId(0);
+//        employeeService.save(employee);
+//    }
+//
+//}
