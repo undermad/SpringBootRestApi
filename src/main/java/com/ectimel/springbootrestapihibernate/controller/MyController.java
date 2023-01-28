ad2ce9015a03dcab7e1152653fd9d2b5cd5281e1
Ectimel <dtworek94@gmail.com>
2023-01-28 22:04:15 +0000

    all thymeleaf templates are done, we have basic crud functionality in browser, and we can do same thing using api

--- src/main/java/com/ectimel/springbootrestapihibernate/controller/MyController.java
+++ src/main/java/com/ectimel/springbootrestapihibernate/controller/MyController.java
@@ -1,18 +1,67 @@
 package com.ectimel.springbootrestapihibernate.controller;
 
+import com.ectimel.springbootrestapihibernate.dao.EmployeeRepository;
+import com.ectimel.springbootrestapihibernate.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MyController {
 
-    @GetMapping("/home")
-    public String home(Model model){
+    private final EmployeeRepository employeeRepository;
+
+    public MyController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
         model.addAttribute("theDate", new Date());
 
         return "home";
     }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Employee> employeeList = employeeRepository.findAllByOrderByLastNameAsc();
+
+        model.addAttribute("employees", employeeList);
+
+        return "list";
+    }
+
+    @GetMapping("/showForm")
+    public String showForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
+        return "show-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/showFormUpdate")
+    public String update(@RequestParam("employeeId") int id, Model model) {
+
+        model.addAttribute("employee", employeeRepository.findById(id).orElse(null));
+        return "show-form-update";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+        employeeRepository.deleteById(id);
+        return "redirect:/list";
+    }
+
+
 }
--- src/main/java/com/ectimel/springbootrestapihibernate/dao/EmployeeRepository.java
+++ src/main/java/com/ectimel/springbootrestapihibernate/dao/EmployeeRepository.java
@@ -4,6 +4,10 @@ import com.ectimel.springbootrestapihibernate.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource (path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAllByOrderByLastNameAsc();
 }
